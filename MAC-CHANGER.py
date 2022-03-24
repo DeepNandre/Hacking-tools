f71f9b1347b9e2edd91d1fd21ac5faea77ab0e9e
Deep Nandre <81618576+Deepn05@users.noreply.github.com>
2022-03-24 19:53:55 +0530

    Delete MAC-CHANGER.py

--- MAC-CHANGER.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-
-subprocess.call("ipconfig wlan0 down", shell=True)
-subprocess.call("ipconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ipconfig wlan0 up", shell=True)
